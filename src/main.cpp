4aeb780a91b5b11a28df3554325d6fb50de8c2a2
Taran Govindu <trex.taran@gmail.com>
2026-02-22 21:22:05 -0500

    Merge older test cases and patch Wikipedia plot data paths

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,11 @@
 #include <iostream>
 #include <vector>
 
+
 #include "burger_scheme.h"
 #include "burgers.h"
 #include "initial_condition_generator.h"
+
 
 int main() {
   // --- WIKIPEDIA TEST CASES ---
@@ -54,4 +56,67 @@ int main() {
     n_wave_solver.solve();
     n_wave_solver.saveSolution("../data_v2", "wiki_n_wave_" + v_str, 10);
   }
+
+  // --- PREVIOUS TEST CASES ---
+  std::cout << "\n--- PREVIOUS TEST CASES ---\n";
+
+  SolverConfig step_function_config = {.kinematic_viscosity = 0.01,
+                                       .time_steps = 2000,
+                                       .domain_length = 2.0,
+                                       .time_step_size = 0.001};
+
+  // 1 everywhere in domain
+  // for 0.5 <= x <= 1 the function value is 2
+  std::function<double(double)> step_function = [](double x) -> double {
+    if (x >= 0.5 && x <= 1.0) {
+      return 2.0;
+    } else {
+      return 1.0;
+    }
+  };
+
+  BurgersSolver1d step_function_solver(std::make_unique<Godunov>(),
+                                       step_function_config, step_function);
+
+  step_function_solver.solve();
+  step_function_solver.saveSolution("../data", "step_function", 1);
+  std::cout << "step function was nan detected: "
+            << step_function_solver.wasNanDetected() << std::endl;
+
+  SolverConfig sine_wave_config = {.kinematic_viscosity = 0.01,
+                                   .time_steps = 5000,
+                                   .domain_length = 2.0 * M_PI,
+                                   .time_step_size = 0.001};
+
+  // initial condition: one full sine wave over [0, 2π]
+  std::function<double(double)> sine_function = [](double x) -> double {
+    return std::sin(x);
+  };
+
+  BurgersSolver1d solver(std::make_unique<Godunov>(), sine_wave_config,
+                         sine_function);
+
+  solver.solve();
+  solver.saveSolution("../data", "sine_wave", 1);
+  std::cout << "sine wave was nan detected: "
+            << step_function_solver.wasNanDetected() << std::endl;
+
+  RandomInitialConditionConfig functionConfig;
+  RandomInitialCondition f(functionConfig, false, true);
+  std::cout << f.toString() << std::endl;
+
+  SolverConfig random_function_config = {
+      .kinematic_viscosity = 0.01,
+      .time_steps = 10000,
+      .domain_length = 10.0,
+      .time_step_size = 0.0001,
+  };
+
+  BurgersSolver1d random_function_solver(std::make_unique<Godunov>(),
+                                         random_function_config, f);
+
+  random_function_solver.solve();
+  random_function_solver.saveSolution("../data", "random_function", 1);
+  std::cout << "random function was nan detected: "
+            << step_function_solver.wasNanDetected() << std::endl;
 }
