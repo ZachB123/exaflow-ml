f3d5c4ef907b91d2385a7c6a4cc8776868e438ce
Eun-Ho Lee <ejlee6712@gmail.com>
2026-03-08 21:43:35 -0400

    Implement FTCS update step for 2D Burgers solver

--- src/schemes/ftcs_2d.cpp
+++ src/schemes/ftcs_2d.cpp
@@ -6,6 +6,17 @@
 void calculateNextU(const std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& u_next, double cq, int nx, int ny, double dt, double dx, double dy, double nu){
     for (int i = 1; i < nx - 1; ++i) {
         for (int j = 1; j < ny - 1; ++j) {
+
+            double du_dx = (u[i + 1][j] - u[i - 1][j]) / (2.0 * dx);
+            double du_dy = (u[i][j + 1] - u[i][j - 1]) / (2.0 * dy);
+
+            double d2u_dx2 = (u[i + 1][j] - 2 * u[i][j] + u[i - 1][j]) / (dx * dx);
+            double d2u_dy2 = (u[i][j + 1] - 2 * u[i][j] + u[i][j - 1]) / (dy * dy);
+
+            double artificial_viscosity = calculateArtificialViscosity(u, cq, dx, dy, i, j, nx, ny);
+
+            u_next[i][j] = u[i][j] - u[i][j] * dt * (du_dx + du_dy) + (nu + artificial_viscosity) * dt * (d2u_dx2 + d2u_dy2);
+                
         }
     }
 
