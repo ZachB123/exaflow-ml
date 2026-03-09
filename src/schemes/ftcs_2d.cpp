82d837a56cc124861a559f4239f0123cf1cecf1d
Eun-Ho Lee <ejlee6712@gmail.com>
2026-03-08 21:54:11 -0400

    Add cmath because std::abs use

--- src/schemes/ftcs_2d.cpp
+++ src/schemes/ftcs_2d.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "burger_scheme_2d.h"
+#include <cmath>
 
 //'regular function instead for now, can redefine when FTCS2D is a class'
 void calculateNextU(const std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& u_next, double cq, int nx, int ny, double dt, double dx, double dy, double nu){
