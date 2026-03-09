56493c1aa0f21f5a6c70ca8c0e967b05ade66875
Eun-Ho Lee <ejlee6712@gmail.com>
2026-03-08 21:27:04 -0400

    Add nested loops for 2D

--- src/schemes/ftcs_2d.cpp
+++ src/schemes/ftcs_2d.cpp
@@ -4,4 +4,9 @@
 
 //'regular function instead for now, can redefine when FTCS2D is a class'
 void calculateNextU(const std::vector<std::vector<double>>& u, std::vector<std::vector<double>>& u_next, double cq, int nx, int ny, double dt, double dx, double dy, double nu){
-    
+    for (int i = 1; i < nx - 1; ++i) {
+        for (int j = 1; j < ny - 1; ++j) {
+        }
+    }
+
+}
