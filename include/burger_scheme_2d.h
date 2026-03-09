a8e94e5a283f2849bb7ace928c90901f14e2c030
Dev Sharma <devsharma@Devs-MacBook-Air-2.local>
2026-03-09 17:03:16 -0400

    Fill out BurgerScheme2D class definition (calculateNextU and calculateArtificalViscosity)

---
+++ include/burger_scheme_2d.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class BurgerScheme2D {
+    public:
+        // destructor
+        virtual ~BurgerScheme2D() = default;
+
+        // function for calculateNextU, implemented in subclasses
+        // = 0 enforces its implementation in the subclasses
+        virtual void calculateNextU(
+                    const std::vector<std::vector<double>>& u, 
+                    std::vector<std::vector<double>>& u_next, 
+                    double cq, 
+                    int num_domain_points_x, 
+                    int num_domain_points_y, 
+                    double time_step_size,
+                    double spatial_step_size_x, 
+                    double spatial_step_size_y, 
+                    double kinematic_viscosity) = 0
+
+        // get the name of the scheme
+        virtual std::string getName() const = 0;
+
+    protected:
+        // function to calculate artifiical viscosity, implemented in subclasses
+        virtual double calculateArtificialViscosity(
+            const std::vector<std::vector<double>>&, // u 
+            double, // cq 
+            double, // spatial_step_size_x
+            double, // spatial_step_size_y
+            int, // i
+            int, // j
+            int, // num_domain_points_x
+            int ) const // num_domain_points_y
+        {
+            return 0.0; // default: no artificial viscosity
+        }
+};
