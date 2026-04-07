a5fe138465f6dfc763397cd1951d6035c0ec1e08
nakulshah04 <shahnakul@gmail.com>
2026-04-07 13:06:31 -0400

    reverse engineering the cq term for ftcs and lax wendroff scehemes

--- src/schemes/ftcs.cpp
+++ src/schemes/ftcs.cpp
@@ -2,37 +2,69 @@
 
 #include "burger_scheme.h"
 
-void FTCS::calculateNextU(const std::vector<double>& u, std::vector<double>& u_next, double cq, int num_domain_points, double time_step_size, double spatial_step_size, double kinematic_viscosity) {
+void FTCS::calculateNextU(const std::vector<double>& u,
+                          std::vector<double>& u_next, double cq,
+                          int num_domain_points, double time_step_size,
+                          double spatial_step_size,
+                          double kinematic_viscosity) {
+  for (int i = 1; i < num_domain_points - 1; ++i) {
+    u_next[i] = u[i] -
+                u[i] * time_step_size / (2.0 * spatial_step_size) *
+                    (u[i + 1] - u[i - 1]) +
+                (kinematic_viscosity +
+                 calculateArtificialViscosity(u, cq, spatial_step_size, i,
+                                              num_domain_points)) *
+                    time_step_size / (spatial_step_size * spatial_step_size) *
+                    (u[i + 1] - 2 * u[i] + u[i - 1]);
+
+    // find cq in terms of everything else
+    double max_ux = 0.0;
     for (int i = 1; i < num_domain_points - 1; ++i) {
-        u_next[i] = u[i]
-            - u[i] * time_step_size / (2.0 * spatial_step_size) * (u[i + 1] - u[i - 1])
-            + (kinematic_viscosity + calculateArtificialViscosity(u, cq, spatial_step_size, i, num_domain_points)) * time_step_size / (spatial_step_size * spatial_step_size)
-                * (u[i + 1] - 2 * u[i] + u[i - 1]);
+      double ux = std::abs((u[i + 1] - u[i - 1]) / (2.0 * spatial_step_size));
+      max_ux = std::max(max_ux, ux);
     }
 
-    // wrap around
-    // for some reason adding this artificial viscosity drastically slows the sim down.
-    // u_next[0] = u[0]
-    //     - u[0] * time_step_size / spatial_step_size * (u[0] - u[num_domain_points - 2])
-    //     + (kinematic_viscosity + calculateArtificialViscosity(u, cq, spatial_step_size, 0, num_domain_points)) * time_step_size / (spatial_step_size * spatial_step_size)
-    //     * (u[1] - 2 * u[0] + u[num_domain_points - 2]);
-    u_next[0] = u[0]
-        - u[0] * time_step_size / (2.0 * spatial_step_size) * (u[1] - u[num_domain_points - 2])
-        + (kinematic_viscosity + calculateArtificialViscosity(u, cq, spatial_step_size, 0, num_domain_points))
-          * time_step_size / (spatial_step_size * spatial_step_size)
-          * (u[1] - 2 * u[0] + u[num_domain_points - 2]);
-    u_next[num_domain_points - 1] = u_next[0];
+    double cq = (1.0 / max_ux) *
+                (1.0 / (2.0 * time_step_size) -
+                 kinematic_viscosity / (spatial_step_size * spatial_step_size));
+
+    // clamping cq so that it's not negative
+    cq = std::max(0.0, cq);
+  }
+
+  // wrap around
+  // for some reason adding this artificial viscosity drastically slows the sim
+  // down. u_next[0] = u[0]
+  //     - u[0] * time_step_size / spatial_step_size * (u[0] -
+  //     u[num_domain_points - 2])
+  //     + (kinematic_viscosity + calculateArtificialViscosity(u, cq,
+  //     spatial_step_size, 0, num_domain_points)) * time_step_size /
+  //     (spatial_step_size * spatial_step_size)
+  //     * (u[1] - 2 * u[0] + u[num_domain_points - 2]);
+  u_next[0] = u[0] -
+              u[0] * time_step_size / (2.0 * spatial_step_size) *
+                  (u[1] - u[num_domain_points - 2]) +
+              (kinematic_viscosity +
+               calculateArtificialViscosity(u, cq, spatial_step_size, 0,
+                                            num_domain_points)) *
+                  time_step_size / (spatial_step_size * spatial_step_size) *
+                  (u[1] - 2 * u[0] + u[num_domain_points - 2]);
+  u_next[num_domain_points - 1] = u_next[0];
 }
 
-double FTCS::calculateArtificialViscosity(const std::vector<double>& u, double cq, double spatial_step_size, int i, int num_domain_points) const {
-    // linear artificial viscosity model, not quadratic RVN
-    double ux = (i == 0) ? (u[i + 1] - u[num_domain_points - 2]) / (2.0 * spatial_step_size) : (u[i + 1] - u[i - 1]) / (2.0 * spatial_step_size);
-    // Only take artificial viscosity when in compression
-    double artificial_viscosity = (ux < 0) ? cq * spatial_step_size * spatial_step_size * std::abs(ux) : 0.0;
+double FTCS::calculateArtificialViscosity(const std::vector<double>& u,
+                                          double cq, double spatial_step_size,
+                                          int i, int num_domain_points) const {
+  // linear artificial viscosity model, not quadratic RVN
+  double ux = (i == 0) ? (u[i + 1] - u[num_domain_points - 2]) /
+                             (2.0 * spatial_step_size)
+                       : (u[i + 1] - u[i - 1]) / (2.0 * spatial_step_size);
+  // Only take artificial viscosity when in compression
+  double artificial_viscosity =
+      (ux < 0) ? cq * spatial_step_size * spatial_step_size * std::abs(ux)
+               : 0.0;
 
-    return artificial_viscosity;
+  return artificial_viscosity;
 }
 
-std::string FTCS::getName() const {
-    return "FTCS";
-}
+std::string FTCS::getName() const { return "FTCS"; }
